01b32a99e4a7028e47de2f82beca4c7b79a3b51f
Balázs Máté <balazsm86@gmail.com>
2016-10-23 21:16:01 +0200

    Added functions and global variables

---
+++ gameInventory.py
@@ -0,0 +1,26 @@
+
+
+# ========== Functions ==========
+
+def display_inventory():
+
+
+def add_to_inventory(inventory, added_items):
+
+
+def print_table(order):
+
+
+def import_inventory(filename):
+
+
+def export_inventory(filename):
+
+
+
+# ========== Global variables ==========
+
+inv = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
+# ========== Main ==========
