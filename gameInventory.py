221ee922ed97ace6cbe75d52277f32659fd7e768
Balázs Máté <balazsm86@gmail.com>
2016-10-27 09:50:02 +0200

    Rewrited add_to_inventory() function

--- gameInventory.py
+++ gameInventory.py
@@ -17,15 +17,10 @@ def display_inventory(inventory):
 
 def add_to_inventory(inventory, added_items):
     for i in range(len(added_items)):
-        if added_items[i] not in inventory:                 # If the item is not in the inventory...
-                    inventory[added_items[i]] = 1           # ...then add it
-                    continue
-        for inv_key, inv_value in inventory.items():
-            if added_items[i] == inv_key:                  # If the list's element is equal to the inventory element...
-                inventory[inv_key] += 1                     # ...then increase its value with 1
-                continue
-            if added_items[i] != inv_key:
-                continue
+        if added_items[i] in inventory:                         # If the item is in the inventory...
+            inventory[added_items[i]] += 1                      # ...then increase its value
+        else:                                                   # If the item is not in the inventory...
+            inventory[added_items[i]] = 1                       # ...then add it
     return inventory
 
 def print_table(order):
@@ -193,11 +188,11 @@ def import_inventory(filename):
 # ========== Main ==========
 
 inv = add_to_inventory(inv, dragon_loot)
-# display_inventory(inv)
+display_inventory(inv)
 # order = None
 # order = "count,desc"
 # order = "count,asc"
 
 # print_table(order)
-import_inventory("import_inventory.csv")
-display_inventory(inv)
+# import_inventory("import_inventory.csv")
+# display_inventory(inv)
